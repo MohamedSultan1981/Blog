86baae1062c28f1037ef2f2eaf9383e8ad07615a
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2022-09-30 21:27:49 +0200

    slug

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -29,4 +29,4 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     def get_absolute_url(self):
-        return reverse('blog:post_detail',args=[self.id])   
+        return reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day,self.slug])  
