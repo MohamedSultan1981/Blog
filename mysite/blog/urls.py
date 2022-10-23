2f945c2ead4b76b1e077e57b45a703baf191e05d
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2022-10-23 16:49:01 +0200

    trigram

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -12,4 +12,5 @@ path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='p
 path('<int:post_id>/share/',views.post_share, name='post_share'),
 path('<int:post_id>/comment/',views.post_comment, name='post_comment'),
 path('feed/', LatestPostsFeed(), name='post_feed'),
+path('search/', views.post_search, name='post_search'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from .models import Post, Comment
 from taggit.models import Tag
 from django.views.generic import ListView
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector,SearchQuery, SearchRank
 # Create your views here.
 class PostListView(ListView):
     """
@@ -100,5 +101,17 @@ def post_comment(request, post_id):
     # Save the comment to the database
         comment.save()
     return render(request, 'blog/post/comment.html',{'post': post,'form': form,'comment': comment})
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate( search=search_vector,rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank') 
+    return render(request,'blog/post/search.html', {'form': form, 'query': query, 'results': results})
         
     
